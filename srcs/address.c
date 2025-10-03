bf1622554a4f9cd3a98935f836bde20012ba16dc
nedd06 <vincent.garcia6942@gmail.com>
2025-10-03 22:55:38 +0200

    debut de conversion mac addr

--- srcs/address.c
+++ srcs/address.c
@@ -56,5 +56,62 @@ bool_t valid_ip_address(char* ip_address)
 
 bool_t valid_mac_address(char* mac_address)
 {
-    return TRUE;
+    struct ifaddrs* ifa_head;
+    struct in_addr  addr_in;
+    unsigned char*  sll_mac_addr;
+
+    sll_mac_addr = mac_str_to_sll(mac_address);
+    if (!sll_mac_addr)
+        return FALSE;
+    if (getifaddrs(&ifa_head) < 0)
+        return FALSE;
+    for (struct ifaddrs *ifa = ifa_head; ifa != NULL; ifa = ifa->ifa_next)
+    {
+        struct sockaddr_ll *ifa_addr = (struct sockaddr_ll*)ifa->ifa_addr;
+
+        if (!ifa_addr)
+            continue ;
+        else if (ft_strcmp(ifa_addr->sll_addr, addr_in))
+        {
+            freeifaddrs(ifa_head);
+            free(sll_mac_addr);
+            return TRUE;
+        }
+    }
+    freeifaddrs(ifa_head);
+    free(sll_mac_addr);
+    return FALSE;
 }
+
+unsigned char* mac_str_to_sll(char *mac_addr)
+{
+    char**         octets;
+    unsigned char* sll_addr;   
+    char           charset[16] = "0123456789abcdef"
+    
+    sll_addr = malloc(sizeof(char) * 8);
+    if (!sll_addr)
+        return NULL;
+    octets = ft_split(mac_addr, ':');
+    if (!octets)
+    {
+        free(sll_addr);
+        return NULL;
+    }
+    for (ssize_t i = 0; i < ft_size(octets); i++)
+    {
+        if (ft_strlen(octets[i]) != 2)
+        {
+            ft_free_tab(octets);
+            free(sll_addr);
+            return NULL;
+        }
+        // fonction qui prend B1 et le convert dans un unsgiend char
+    }
+}
+
+/*
+
+
+
+*/
--- srcs/main.c
+++ srcs/main.c
@@ -12,6 +12,5 @@ int main(int argc, char **argv)
     {
         return 1;
     }
-    if (valid_ip_address(argv[1]))
     return 0;
 }
